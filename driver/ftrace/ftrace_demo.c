ed2de11c4b306f5472c3d6892d380c8b752f96e6
Your Name <you@example.com>
2019-01-11 03:09:29 -0800

    添加  ftrace trace_printk demo

---
+++ driver/ftrace/ftrace_demo.c
@@ -0,0 +1,24 @@
+/*
+ * ftrace_demo.c
+ */
+ #include <linux/init.h>
+ #include <linux/module.h>
+ #include <linux/kernel.h>
+
+ MODULE_LICENSE("GPL");
+
+ static int ftrace_demo_init(void)
+ {
+	 trace_printk("Can not see this in trace unless loaded for the second time\n");
+     tracing_off();
+	 return 0;
+ }
+
+ static void ftrace_demo_exit(void)
+ {
+	 trace_printk("Module unloading\n");
+     tracing_off();
+ }
+
+ module_init(ftrace_demo_init);
+ module_exit(ftrace_demo_exit);
